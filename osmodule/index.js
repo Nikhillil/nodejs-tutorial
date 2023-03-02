50f028df6bf4b6a61a58087abc880222037aa9c3
nikhil <nikhillilwani999@gmail.com>
2023-03-02 13:08:58 +0530

    osmodule

---
+++ osmodule/index.js
@@ -0,0 +1,7 @@
+const os = require("os");
+
+// os.freemem() --> return karta hai free memory hamare system ki
+console.log(os.freemem());
+
+// os.homedir() --> return home directory
+console.log(os.homedir());
